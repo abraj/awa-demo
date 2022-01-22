2d458fbcb60b07016cd54c803c5e0cc4551f4194
Abhishek Raj <abhi@raj.me>
2022-01-22 20:47:54 +0530

    Initial cleanup

---
+++ src/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="App-title">Web APIs Demo</div>
+        <br />
+        <div>
+          <a className="App-link" href="/contact-picker-demo">Contact Picker API Demo</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
